6494e362a2b41136357cd5bf20e2f5c3e46ee4b2
neo <elezi.mentor@gmail.com>
2024-12-04 19:41:52 +0100

    idk login

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,22 @@
 'use client';
-
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignIn routing="hash" />
+    <div className="flex h-max min-h-[60vh] py-20 items-center justify-center bg-gray-50">
+      <SignIn
+        appearance={{
+          elements: {
+            card: 'shadow-lg',
+          },
+        }}
+        routing="hash"
+      />
+      <style jsx global>{`
+        .cl-internal-b3fm6y {
+          display: none !important; /* Hides the badge */
+        }
+      `}</style>
     </div>
   );
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,22 @@
 'use client';
-
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignUp routing="hash" />
+    <div className="flex h-max min-h-[60vh] py-20 items-center justify-center bg-gray-50">
+      <SignUp
+        appearance={{
+          elements: {
+            card: 'shadow-lg',
+          },
+        }}
+        routing="hash"
+      />
+      <style jsx global>{`
+        .cl-internal-b3fm6y {
+          display: none !important; /* Hides the badge */
+        }
+      `}</style>
     </div>
   );
 }
