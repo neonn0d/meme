8e435d045c73f70f8133edb75c8b1615fd4ec58f
neo <elezi.mentor@gmail.com>
2024-12-05 13:56:40 +0100

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
-import { ClientLayout } from '@/components/ClientLayout'
-import { Analytics } from "@vercel/analytics/react"
-import './globals.css'
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { ClientLayout } from "@/components/ClientLayout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = { 
-  title: 'BUIDL - Memecoin Website Generator',
-  description: 'Create your own customized memecoin website in minutes with BUIDL',
-}
+export const metadata = {
+  title: "BUIDL - Memecoin Website Generator",
+  description:
+    "Create your own customized memecoin website in minutes with BUIDL",
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -24,9 +26,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ClientLayout>
             {children}
             <Analytics />
+            <SpeedInsights />
           </ClientLayout>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
